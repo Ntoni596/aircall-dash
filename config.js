6cd24f8b4fede4741036456156eb13d0e7ef9ecd
Ntoni596 <68333457+Ntoni596@users.noreply.github.com>
2025-04-02 07:52:37 +0000

    Deploying to gh-pages from @ Ntoni596/aircall-dash@0120f667d7c153a1094e4370dc0f4529f5de592c 🚀

---
+++ config.js
@@ -0,0 +1,11 @@
+// This file is auto-generated - DO NOT EDIT
+const firebaseConfig = {
+  apiKey: "AIzaSyAJXH9yHOhD2XLCMRIVDM7kgxR0db2TiO4",
+  authDomain: "aircalldata.firebaseapp.com",
+  databaseURL: "https://aircalldata-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "aircalldata",
+  storageBucket: "aircalldata.firebasestorage.app",
+  messagingSenderId: "630404917854",
+  appId: "1:630404917854:web:7a18eb351e9bc2ef35a2cf",
+  measurementId: "G-0DRN6VNJXS"
+};
